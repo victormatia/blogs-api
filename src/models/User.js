bc4d852f253bbc6c980d432381374c22fa4657b4
Victor Matias <fvictor1705@gmail.com>
2022-11-30 18:32:11 -0300

    feat: solves requirement 2

---
+++ src/models/User.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    sequelize,
+    underscored: true,
+  });
+
+  return user;
+};
